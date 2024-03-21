1be166246c1ad7aa64cf325c8f0b14a11ed95541
Snehankita Kakade <kakadesnehankita9810@gmail.com>
2024-03-21 15:39:39 +0530

    Java Basics-datatypes in Java

--- JavaBasics.java
+++ JavaBasics.java
@@ -3,7 +3,23 @@ public class JavaBasics{
     public static void main(String args[]){
 
         // output in java
-        System.out.println("Java Programming");
-       
+        System.out.println("\nJava Programming");
+
+        // Primitive datatypes in java
+
+        byte a=8;
+        int b=0;
+        String name="Snehankita";
+        char c='a';
+        boolean status=true;
+        float marks=98.23f;
+        //to store large integer long  & double datatype used
+        long height=250000000;  
+        double population=1200005550;
+
+        System.out.println("a = "+a+"\nb = "+b+"\nc = "+c+"\nStatus = "+status+"\nmarks = "+marks+"\nheight = "+height+"\nPopulation = "+population);
+    
+
+
     }
 }
